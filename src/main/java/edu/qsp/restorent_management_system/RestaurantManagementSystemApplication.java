f269c6ae321cfa3a24a69a20c28ee5dff1d68250
sagar kumar <81793637+sagar56780@users.noreply.github.com>
2024-05-20 18:07:01 +0530

    first commit

---
+++ src/main/java/edu/qsp/restorent_management_system/RestaurantManagementSystemApplication.java
@@ -0,0 +1,17 @@
+package edu.qsp.restorent_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagementSystemApplication  {
+
+  
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantManagementSystemApplication.class, args);
+        System.err.println("i am in spring boot");
+    }
+
+ 
+
+}
---
+++ src/test/java/edu/qsp/restorent_management_system/RestorentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.qsp.restorent_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestorentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
